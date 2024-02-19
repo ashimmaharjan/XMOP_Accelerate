0281e0d85d42a4735388fcf242b8428f54b34f37
Ashim Maharjan <ciferulquiorra008@gmail.com>
2024-02-19 12:43:22 +1100

    History page updated with Deployment name and status columns

--- app/dashboard/history/page.jsx
+++ app/dashboard/history/page.jsx
@@ -4,32 +4,44 @@ const History = () => {
   const deployments = [
     {
       id: 1,
+      name: "Deployment 1",
       architectureType: "Monolith",
+      status: "Deployed",
       deploymentDate: "Monday, Feb 10, 2023",
     },
     {
       id: 2,
+      name: "Deployment 2",
+      status: "Deployed",
       architectureType: "Monolith",
       deploymentDate: "Tuesday, Feb 11, 2023",
     },
     {
       id: 3,
+      name: "Deployment 3",
       architectureType: "Monolith",
+      status: "Deployed",
       deploymentDate: "Wednesday, Feb 12, 2023",
     },
     {
       id: 4,
+      name: "Deployment 4",
       architectureType: "Microservice",
+      status: "In Progress",
       deploymentDate: "Thursday, Feb 13, 2023",
     },
     {
       id: 5,
+      name: "Deployment 5",
       architectureType: "Microservice",
+      status: "Deployed",
       deploymentDate: "Friday, Feb 14, 2023",
     },
     {
       id: 6,
+      name: "Deployment 6",
       architectureType: "Lightsail",
+      status: "Failed",
       deploymentDate: "Saturday, Feb 15, 2023",
     },
   ];
@@ -46,9 +58,11 @@ const History = () => {
         Let&apos;s take a look at your recent deployments.
       </span>
 
-      <div className="grid grid-cols-3 gap-3 shadow-inner justify-between mt-5 bg-sky-200 px-5 py-3 text-gray-700 font-bold">
+      <div className="grid grid-cols-5 gap-3 shadow-inner justify-between mt-5 bg-sky-200 px-5 py-3 text-gray-700 font-bold">
         <div className="col-span-1">S.N.</div>
+        <div className="col-span-1">Name</div>
         <div className="col-span-1">Architecture Type</div>
+        <div className="col-span-1">Status</div>
         <div className="col-span-1">Deployed Date</div>
       </div>
 
@@ -56,9 +70,10 @@ const History = () => {
       {deployments.map((deployment) => (
         <div
           key={deployment.id}
-          className="grid grid-cols-3 gap-3 drop-shadow-lg shadow-sm justify-between px-5 py-3 text-gray-700 border-b"
+          className="grid grid-cols-5 gap-3 drop-shadow shadow-sm justify-between px-5 py-3 text-gray-700 border-b"
         >
           <div className="col-span-1">{deployment.id}</div>
+          <div className="col-span-1">{deployment.name}</div>
           <div className="col-span-1">
             <span
               className={`px-4 py-1 text-gray-50 rounded-3xl ${
@@ -70,6 +85,19 @@ const History = () => {
               }`}
             >
               {deployment.architectureType}
+            </span>
+          </div>
+          <div className="col-span-1">
+            <span
+              className={`px-4 py-1 border-2 rounded-3xl ${
+                deployment.status === "Deployed"
+                  ? "text-green-600 border-green-500"
+                  : deployment.status === "In Progress"
+                  ? "text-blue-500 border-blue-500"
+                  : "text-red-500 border-red-500"
+              }`}
+            >
+              {deployment.status}
             </span>
           </div>
           <div className="col-span-1">{deployment.deploymentDate}</div>
